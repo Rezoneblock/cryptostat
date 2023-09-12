21cc7099195b3ebacdf4d6f988d71654f66a0031
Timur <trew.tr2019@yandex.ru>
2023-09-12 17:35:46 +0300

    ultramegafinaltry3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>Hello, World!1234</h1>
+    <h1>Hello, World!123412341234</h1>
   </div>
 </template>
 
@@ -8,5 +8,6 @@
 export default {
   name: 'HelloWorld',
 }
+console.log('rofl');
 </script>
 
